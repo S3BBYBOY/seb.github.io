617538098c6e288e50b0e9b0455894e87f6c2e55
S3BBYBOY <167453190+S3BBYBOY@users.noreply.github.com>
2024-11-04 16:17:20 +0800

    svelte update

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,29 @@
+<h1 class="text-4xl text-center mb-4">About</h1>
+<div class="flex flex-col items-center"></div>
+  
+<div>
+    <img src="pau1.jpg" alt="pawwii" class="w-full h-auto max-w-sm rounded-lg mx-auto" />
+</div>
+<div class="w-full max-w-2xl">
+    <div class="flex flex-col gap-4 text-gray-300">
+        
+		<p>
+			Hi, I'm Ze-v De Guzman Jardin. I'm currently a 3rd-year college student pursuing a Bachelor of Science 
+            in Information Technology at Gordon College.
+			Information Technology wasn't my first course choice because my dream was to become a Culinary student, 
+            but for some reason, I ended up in technology. 
+			Now, my dream is to become a web developer and gain more knowledge about the tech industry. 
+			However, I'm not yet confident in my skills and know I still have a lot to learn in IT.
+		</p>
+
+		<p>
+			I'm just a simple guy who enjoys the outdoors, and has a passion for sports like basketball and bowling
+             all while cherishing time spent with friends
+		</p>
+
+		<p>I am excited about the future and envision myself becoming a web developer in the coming years. 
+            This portfolio will showcase my achievements and successes throughout my journey. 
+            I believe that nothing is impossible—have faith, trust in God, and know that you can achieve your goals.
+		</p>
+	</div>
+</div>
---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	const portfolioImages = [
+		{
+			alt: "Joining Webinar to gain more knowledge ",
+			src: "Portfolio/8-bit-clarity.jpg",
+		},
+		{
+			alt: "Application Development Blog project",
+			src: "Portfolio/blogproject.jpg",
+		},
+		{
+			alt: "Application Development Blog projec",
+			src: "Portfolio/blogproject1.jpg",
+		},
+		{
+			alt: "Latest sports ",
+			src: "Portfolio/bowling.jpg",
+		} 
+	];
+</script>
+
+<h1 class="text-4xl text-center">Portfolio</h1>
+<p class="text-center mb-8">Here are my Project, Activities and Sports that I have worked on:</p>
+<div class="w-full max-w-4xl mx-auto">
+    <div class="grid gap-8 grid-cols-1 md:grid-cols-2">
+        {#each portfolioImages as portfolioImage (portfolioImage.src)}
+        <div class="w-full h-fit overflow-hidden rounded-lg text-center">
+            <img 
+                src={portfolioImage.src} 
+                alt={portfolioImage.alt} 
+                class="w-full h-80 object-contain hover:scale-110 transition-transform duration-300 mx-auto" 
+            >
+            <p class="mt-4 text-lg">{portfolioImage.alt}</p>
+        </div>
+        {/each}
+    </div>
+</div>
