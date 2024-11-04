cb0e95f273c9973bccc7d978010a637eee548a67
S3BBYBOY <167453190+S3BBYBOY@users.noreply.github.com>
2024-11-04 16:24:13 +0800

    update svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w-full max-w-2xl grid md:grid-cols-2 gap-4">
+	<div>
+		<img src="ze-v.jpg" alt="seb" class="w-full h-auto max-w-sm rounded-lg mx-auto" />
+	</div>
+	<div class="flex flex-col gap-3 justify-center">
+		<h1 class="text-4xl text-primary-300">Welcome!</h1>
+		<p>Hi, Ze-v De Guzman Jardin. I'm currently a 3rd-year college student pursuing a Bachelor of Science 
+            in Information Technology at Gordon College.</p>
+		<p>
+			I live in Olongapo City, Zambales. 
+		</p>
+		<p>
+			Soon to be Web Developer. 
+		</p>
+	</div>
+</div>
